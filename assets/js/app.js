313992d1f0cfc23224d7d41de3856020917b3a9e
makaulucky <makaulucky5@gmail.com>
2023-07-23 13:23:40 +0300

    required form fileds

--- assets/js/app.js
+++ assets/js/app.js
@@ -135,17 +135,17 @@ let footer = $(`
           
                 <div class="formcarry-block">
                   <label for="fc-generated-1-name">Full Name</label>
-                  <input type="text" name="name" id="fc-generated-1-name" placeholder="Your first and last name" />
+                  <input type="text" name="name" id="fc-generated-1-name" required placeholder="Your first and last name" />
                 </div>
                 
                 <div class="formcarry-block">
                   <label for="fc-generated-1-email">Your Email Address</label>
-                  <input type="email" name="email" id="fc-generated-1-email" placeholder="john@doe.com" />
+                  <input required type="email" name="email" id="fc-generated-1-email" placeholder="john@doe.com" />
                 </div>
                 
                 <div class="formcarry-block">
                   <label for="fc-generated-1-message">Your message</label>
-                  <textarea name="message" name="message" id="fc-generated-1-message" placeholder="Enter your message..."></textarea>
+                  <textarea required name="message" name="message" id="fc-generated-1-message" placeholder="Enter your message..."></textarea>
                 </div>
                 
                 <div class="formcarry-block">  
